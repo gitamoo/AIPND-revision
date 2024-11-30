33c7a5f7675b0e376a174f5a9f5a835b16c9f396
rita <ritagitamo@gmail.com>
2024-12-01 00:51:18 +0300

    Update get_input_args.py

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: Sai Mounika Balla
-# DATE CREATED: 10/11/2024                                 
+# PROGRAMMER: Rita Gitamo
+# DATE CREATED: 30/11/2024                                 
 # REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
